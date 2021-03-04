36c1bc60396df2210e9f8fe8ea522d989ad3b7ca
Bolko Schreiber <bolko_schreiber@cotyinc.com>
2021-03-04 08:53:25 +0100

    Error handling in fetch methods & TCP port configurable

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const fetch = require("node-fetch");
 const NodeCache = require("node-cache");
 
 const app = express();
-const port = 3000;
-const cacheFile = "cache.json";
-const metaFile = "meta.json";
 
+const port = parseInt(process.env.OWP_PORT) || 3000;
 const maxCacheAge = parseInt(process.env.OWP_CACHE_DURATION) || 60;
 const apiKey = process.env.OWP_OPEN_WEATHER_MAP_API_KEY || "";
 const city = process.env.OWP_CITY || "Köln";
@@ -17,25 +15,37 @@ const exclude = process.env.OWP_EXCLUDE;
 const mycache = new NodeCache({
   stdTTL: maxCacheAge,
   deleteOnExpire: true,
-  useClones: true,
+  useClones: false,
 });
+
+const checkStatus = async res => {
+	if (res.ok) {
+		return res;
+	} else {
+    var errorJson = await res.json()
+		throw new Error(`HTTP Error Response: ${res.status} ${res.statusText} <br> Message from openweathermap.org: ${errorJson.message}`);
+	}
+}
 
 async function tryFetch(uri) {
   try {
     const res = await fetch(encodeURI(uri));
-    
-    return await res.json();
+    await checkStatus(res)    
+    return {json: await res.json()};
   } catch (err) {
-    return err;
+    return {err: err.message};
   }
 }
 
 async function getLatLon() {
-  const json = await tryFetch(
+  const {err, json} = await tryFetch(
     `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`
   );
 
-  return { lat: json[0].lat, lon: json[0].lon };
+  if (!err) {
+    return { lat: json[0].lat, lon: json[0].lon };
+  }
+  return {err: err}
 }
 
 async function getWeatherData({ lat, lon }) {
@@ -64,20 +74,34 @@ app.get("/", async (req, res) => {
     console.log(
       `No cached coords found for ${city} - fetching new geo location.`
     );
-    const { lat, lon } = await getLatLon();
+    const { err, lat, lon } = await getLatLon();
+    if (!err) {
     console.log(
       `Adding geo location for ${city} to cache. Lat: ${lat} Lon: ${lon}`
     );
     geoLocation = { lat: lat, lon: lon };
     mycache.set(locationCacheKey, geoLocation, 86400);
+    }
+    else {
+      console.log(err);
+      res.send(err);
+      return;
+    }
   }
 
   var payload = mycache.get(payloadCacheKey);
 
   if (!payload) {
     console.log(`No cached weather data found for ${city} - fetching new weather data`);
-    payload = await getWeatherData(geoLocation);
-    mycache.set(payloadCacheKey, payload);
+    var {err, json} = await getWeatherData(geoLocation);
+    if (!err) {
+      payload = json
+      mycache.set(payloadCacheKey, payload);
+    } else {
+      console.log(err)
+      res.send(err);
+      return;
+    }
   } else {
     console.log(`Sending cached weather data for ${city}`)
   }
